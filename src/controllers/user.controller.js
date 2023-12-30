bd6cf7b69c675031977847d85578ba535fc6ba88
mandloiabhi <38561149+mandloiabhi@users.noreply.github.com>
2023-12-30 19:24:40 +0000

    commit 1st dec/31

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,44 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-
-
+import {ApiError} from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { ApiResponse}  from "../utils/ApiResponse.js";
 const registerUser= asyncHandler( async (req,res) => {
-    res.status(200).json({message:"Ok"})
+    
+    // res.status(200).json({message:"Ok"})
+    // console.log("request comes");
+
+    const {username,email,role,password}=req.body;
+    // from reqest body we are taking this thing it  // take care that name should be same as key value of in json object of request
+
+    if([username,email,role,password].some((field)=>field?.trim()===""))
+    {
+        throw new ApiError(400,"all fields are required");
+    }
+     
+    const ExistedUser=User.find({
+        $or: [{username},{email}]
+    });
+    if(ExistedUser)
+    {
+        throw new ApiError(409,"user is already existed");
+    }
+    // creat new resource as user in
+    const user= await User.create({
+        username : username.toLowerCase(),
+        email,
+        role,
+        password
+    });
+    const createdUser= await User.findById(user._id).select("-password -refreshToken");  // it check wether the user is created or not in database by using the findById to check wether the user is present or not
+    // above select will not allowed password and refreshToken to be stored in createdUser 
+    if(!createdUser)
+    {
+        throw new ApiError(500,"something went wrong in creating user");
+    }
+
+     return res.status(201).json(
+        new ApiResponse(200,createdUser,"User registered successfully")
+     );
 });
 
 export {registerUser};
