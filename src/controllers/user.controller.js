57cbca2465d4a69c2ef387b1416f40ed0f11395e
mandloiabhi <38561149+mandloiabhi@users.noreply.github.com>
2024-01-03 09:45:46 +0000

    created login user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,28 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { ApiResponse}  from "../utils/ApiResponse.js";
-const registerUser = asyncHandler( async (req, res) => {
+const generateAccessTokenandRefreshToken= async (Userid) =>
+{
+
+  try 
+  {
+    const user= await User.findById(Userid);
+    const accessToken=user.generateAccessToken();
+    const refreshToken=user.generateRefreshToken();
+    user.refreshToken=refreshToken;
+    await user.save({validateBeforeSave: false});  // saving refresh token in database
+    return {accessToken,refreshToken};
+
+
+  } catch (error) 
+  {
+    throw new ApiError(500,"SOME thing wrong in while geneating refresh and access token");
+  }
+}
+
+
+
+const registerUser = asyncHandler( async (Userid) => {
     // get user details from frontend
     // validation - not empty
     // check if user already exists: username, email
@@ -73,47 +94,39 @@ const registerUser = asyncHandler( async (req, res) => {
     )
 
 } )
-// const registerUser= asyncHandler( async (req,res) => {
-    
-//     // res.status(200).json({message:"Ok"})
-//     // console.log("request comes");
 
-//     const {username,email,role,password}=req.body;
-//     console.log(req.body);
-//     // from reqest body we are taking this thing it  // take care that name should be same as key value of in json object of request
-    
-//     if([username,email,role,password].some((field)=>field?.trim()===""))
-//     {
-//         throw new ApiError(400,"all fields are required");
-//     }
-//     // const ExistedUser= await User.find({username});
-//     const existedUser = await User.findOne({
-//         $or: [{ username }, { email }]
-//     })
-    
-//     if(existedUser)
-//     {
-//         throw new ApiError(409,"user is already existed");
-//     }
-//     // creat new resource as user in
-//     console.log(email);
-//     const user= await User.create({
-//         username : username,
-//         email,
-//         role,
-//         password,
-//     });
-//     console.log("abhijeeet");
-//     const createdUser= await User.findById(user._id).select("-password -refreshToken");  // it check wether the user is created or not in database by using the findById to check wether the user is present or not
-//     // above select will not allowed password and refreshToken to be stored in createdUser 
-//     if(!createdUser)
-//     {
-//         throw new ApiError(500,"something went wrong in creating user");
-//     }
+const loginUser= asyncHandler(async (req,res)=>{
+    // get data from request
+    const {username,password}=req.body;
+    if(!username)
+    {
+        throw new ApiError("400","user name is required to log in");
+    }
 
-//     //  return res.status(201).json(
-//     //     new ApiResponse(200,createdUser,"User registered successfully")
-//     //  );
-// });
+    const  user= await User.findOne({username});
+    if(!user)
+    {
+        throw new ApiError("400","User does not exit")
+    }
+   const ispasswordcorrect= await user.isPasswordCorrect(password);
 
-export {registerUser};
+   if(!ispasswordcorrect)
+   {
+    throw new ApiError("400","password is wrong")
+   }
+
+   const {accessToken,refreshToken}= await generateAccessTokenandRefreshToken(user._id);
+
+   const loggedInUser= await User.findById(user._id).select("-password -refresToken");  // return user object / doc with not having password and refresTOken field 
+
+
+   const options={
+    httpOnly:true,
+    secure:true
+   }
+   
+   return res.status(200).cookie("accessToken",accessToken,options).cookie("refreshToken",refreshToken,options).json(new ApiResponse(200,{user:loggedInUser,accessToken,refreshToken},"user loggen in successfully"));
+   
+})
+
+export {registerUser,loginUser};
